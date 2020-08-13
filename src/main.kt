c27b85245c3fb90a038931d61abc34fe3fd95f38
Gabriel Tobias <gabrielvieiratobias@gmail.com>
2020-08-12 21:05:45 -0300

    Commit inicial

---
+++ src/main.kt
@@ -0,0 +1,9 @@
+fun main(){
+    val titular: String = "Gabriel"
+    val numeroConta: Int = 2300;
+    var saldo: Double = 0.0;
+    saldo = 100.01;
+    println("Titular: $titular")
+    println("Número da conta: $numeroConta")
+    println("Saldo da conta: $saldo")
+}
