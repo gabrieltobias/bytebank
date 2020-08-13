d340e9df250122d20a983b0e1186a62308d3b086
Gabriel Tobias <gabrielvieiratobias@gmail.com>
2020-08-12 21:22:14 -0300

    Adicionando valores

--- src/main.kt
+++ src/main.kt
@@ -2,7 +2,8 @@ fun main(){
     val titular: String = "Gabriel"
     val numeroConta: Int = 2300;
     var saldo: Double = 0.0;
-    saldo = 100.01;
+        saldo = 100.01 + 200;
+        saldo+=300;
     println("Titular: $titular")
     println("Numero da conta: $numeroConta")
     println("Saldo da conta: $saldo")
