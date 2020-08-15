43aaae7cf9f495c29248f7218de32cc7623f6e71
Gabriel Tobias <gabrielvieiratobias@gmail.com>
2020-08-15 10:27:59 -0300

    Estrutura de repetcoes

--- src/main.kt
+++ src/main.kt
@@ -1,14 +1,39 @@
-fun main(){
-    val titular: String = "Gabriel"
-    val numeroConta: Int = 2300;
-    var saldo: Double = 0.0;
-        saldo = 100.01 + 200;
-        saldo -= 1000;
+fun main() {
+    var i = 0;
+    while (i < 5) {
 
-    println("Titular: $titular")
-    println("Numero da conta: $numeroConta")
-    println("Saldo da conta: $saldo")
+        val titular: String = "Gabriel $i"
+        val numeroConta: Int = 1000 + i;
+        var saldo: Double = i + 10.0;
+        println("Titular: $titular");
+        println("Conta: $numeroConta");
+        println("Saldo: $saldo");
+        i++;
+    }
+//    for(i in 8 downTo 1) {
+//    if(i==4){
+//        break;
+//    }
+//    val titular: String = "Gabriel $i"
+//    val numeroConta: Int = 1000 + i;
+//    var saldo: Double = i + 10.0;
+//
+//        println("Titular: $titular");
+//        println("Conta: $numeroConta");
+//        println("Saldo: $saldo");
+//    }
 
+}
+
+fun testaCondicoes(saldo: Double) {
+    //if
+    if (saldo > 0.0) {
+        println("Saldo positivo")
+    } else if (saldo == 0.0) {
+        println("Saldo neutro")
+    } else {
+        println("Saldo negativo")
+    }
     //When
     when {
         saldo > 0.0 -> println("Saldo positivo");
