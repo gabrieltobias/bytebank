aca3f483cd4318e39ed7452b13c375c20681188d
Gabriel Tobias <gabrielvieiratobias@gmail.com>
2020-08-15 10:08:03 -0300

    Adicionando estrutura WHEN

--- src/main.kt
+++ src/main.kt
@@ -3,8 +3,16 @@ fun main(){
     val numeroConta: Int = 2300;
     var saldo: Double = 0.0;
         saldo = 100.01 + 200;
-        saldo+=300;
+        saldo -= 1000;
+
     println("Titular: $titular")
     println("Numero da conta: $numeroConta")
     println("Saldo da conta: $saldo")
+
+    //When
+    when {
+        saldo > 0.0 -> println("Saldo positivo");
+        saldo == 0.0 -> println("Saldo neutro")
+        else -> println("Saldo negativo")
+    }
 }
